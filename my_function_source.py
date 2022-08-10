c04e8e11fc621399badef59cfe8689b80b096772
fw26koje <110898136+fw26koje@users.noreply.github.com>
2022-08-10 20:17:37 +0200

    Update my_function_source.py

--- my_function_source.py
+++ my_function_source.py
@@ -1,5 +1,11 @@
-"""module add function"""
+"""contains code under test for solution_ci"""
 def add_function(a,b):
+    """
+    Add two variables.
+    
+    Args
+       a: first variable
+       b: second variable 
+    """
     return a+b
-"""the function returns the sum of two variables"""
 
